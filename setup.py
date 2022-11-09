e9099a161aa1783c0bba96606867519266a79f72
felgabeee <66903086+felgabeee@users.noreply.github.com>
2022-11-09 09:21:45 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="DD",
+    version="0.0.1",
+    description="Collect DD online",
+    long_description=readme(),
+    long_description_content_type="",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/RobertoPrevato/FooDemo",
+    author="felgabe",
+    author_email="felix.gabet@edhec.com",
+    keywords="demo project",
+    license="MIT",
+    packages=find_packages(),,
+    include_package_data=True,
+)
